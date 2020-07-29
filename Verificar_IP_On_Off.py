6e3c6067384dd7fcebd7562877cc887978b636fc
Felipe Gaudêncio Rocha <52244744+felipegaudencio@users.noreply.github.com>
2020-07-29 19:59:27 -0300

    Verificar_IP_On_Off
    Utilizando as bibliotecas "subprocess" e "os" para verificar os IPs se estão On ou Off de uma sub-rede e salvando nos arquivos On.txt e Off.txt.

--- Verificar_IP_On_Off.py
+++ Verificar_IP_On_Off.py
@@ -7,9 +7,9 @@ with open(os.devnull, "wb") as limbo:
         result=subprocess.Popen(["ping", "-n", "1", "-w", "200", ip],
         stdout=limbo, stderr=limbo).wait()
         if result:
-            arq2 = open("C:/Users/adminbh/Desktop/Estudo/DATASCIENCE/PythonFundamentos-master/Cap04/Notebooks/arquivos/off.txt", "a")
+            arq2 = open("C:/Python38/test/off.txt", "a")
             arq2.write("192.168.1.{0}\n".format(n))
         else:
-            arq2 = open("C:/Users/adminbh/Desktop/Estudo/DATASCIENCE/PythonFundamentos-master/Cap04/Notebooks/arquivos/on.txt", "a")
+            arq2 = open("C:/Python38/test/on.txt", "a")
             arq2.write("192.168.1.{0}\n".format(n))
             arq2.close()
